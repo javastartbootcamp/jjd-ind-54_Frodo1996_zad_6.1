8a10b81ffa844b2ba56a78ab5cd583de78f10e87
Krzysiek Przystarz <frodson35@gmail.com>
2022-12-22 19:35:20 +0100

    Poprawa zadania 6.1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int userNumber;
+        boolean incorrectNumber;
         System.out.println("Zgadnij liczbę z przedziału [100, 200]");
         do {
             System.out.println("Podaj liczbę");
             userNumber = sc.nextInt();
+            incorrectNumber = userNumber < 100 || userNumber > 200 || userNumber % 3 == 1 || userNumber % 3 == 2;
             if (userNumber < 100) {
                 System.out.println("Podana liczba jest za mała.");
             } else if (userNumber > 200) {
@@ -17,7 +19,7 @@ public class Main {
             } else if (userNumber % 3 == 1 || userNumber % 3 == 2) {
                 System.out.println("Podana liczba nie jest podzielna przez 3");
             }
-        } while (userNumber < 100 || userNumber > 200 || userNumber % 3 != 0);
+        } while (incorrectNumber);
         System.out.println("Twoja liczba jest ok.");
         sc.close();
     }
